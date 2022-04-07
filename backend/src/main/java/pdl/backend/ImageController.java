073a47a25cc1e99b3c8c57b2f9fb2dffad8d7fe8
Mathis VACHER <mathis.vacher@etu.u-bordeaux.fr>
2022-04-07 15:22:08 +0200

    Supression warning

--- backend/src/main/java/pdl/backend/ImageController.java
+++ backend/src/main/java/pdl/backend/ImageController.java
@@ -2,10 +2,8 @@ package pdl.backend;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.StackWalker.Option;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -17,10 +15,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-import org.apache.catalina.valves.rewrite.QuotedStringTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -38,7 +33,6 @@ import boofcv.io.image.ConvertBufferedImage;
 import boofcv.io.image.UtilImageIO;
 import boofcv.struct.border.BorderType;
 import boofcv.struct.image.GrayU8;
-import boofcv.struct.image.ImageAccessException;
 import boofcv.struct.image.Planar;
 import java.awt.image.BufferedImage;
 
